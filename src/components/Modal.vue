a8c8ae473ca8b52c8726825e8b74b2a24b5a44b2
DimosSidiropoulos <49340333+DimosSidiropoulos@users.noreply.github.com>
2022-04-13 23:07:16 +0300

    added few things

---
+++ src/components/Modal.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>You have won {{ earnings }}</h1>
+    <button>Save to History</button>
+    <button @click="backHome">Go Back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["earnings"],
+  methods: {
+    backHome() {
+      this.$router.push("Home");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
