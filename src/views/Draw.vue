cc4667e2caccf28c3ac1af79c59ba4c471f35c9b
DimosSidiropoulos <dimos1710@gmail.com>
2022-04-13 16:59:50 +0300

    Draw part1

--- src/views/Draw.vue
+++ src/views/Draw.vue
@@ -5,30 +5,48 @@
       Draw
     </h1>
     <div class="grid grid-cols-2">
-      <!-- <div v-for="number in winningNumbers" :key="number"> -->
-      <h1>This is a test</h1>
-    </div>
-    <div class="flex">
-      <div
-        v-for="number in getNumbers"
-        :key="number"
-        class="
-          bg-red-500
-          text-2xl
-          pt-4
-          m-3
-          w-16
-          h-16
-          rounded-full
-          text-white
-          font-bold
-        "
-      >
-        {{ number }}
+      <div class="flex">
+        <div
+          v-for="number in winningNumbers"
+          :key="number"
+          class="
+            bg-red-500
+            text-2xl
+            pt-4
+            m-3
+            w-16
+            h-16
+            rounded-full
+            text-white
+            font-bold
+          "
+          :class="winner ? 'win' : ''"
+        >
+          {{ number }}
+        </div>
+      </div>
+      <div class="flex">
+        <div
+          v-for="number in getNumbers"
+          :key="number"
+          class="
+            bg-red-500
+            text-2xl
+            pt-4
+            m-3
+            w-16
+            h-16
+            rounded-full
+            text-white
+            font-bold
+          "
+        >
+          {{ number }}
+        </div>
       </div>
     </div>
+    <div v-if="winCount > 2">Κέρδισες</div>
   </div>
-  <!-- </div> -->
 </template>
 
 <script>
@@ -39,12 +57,46 @@ export default {
   data() {
     return {
       winningNumbers: [],
+      test: [],
+      winCount: 0,
     };
   },
-  methods: {},
+  methods: {
+    winner(number) {
+      this.getNumbers.some((r) => {
+        if (r == number) {
+          this.test.push(r); //number?
+          this.winCount++;
+          return true;
+        } else {
+          return false;
+        }
+      });
+      console.log(this.test);
+      console.log(this.winCount);
+    },
+    draw() {
+      for (let i = 0; i < 5; i++) {
+        let number = this.generateNumber();
+        this.winningNumbers.push(number);
+        console.log(this.winningNumbers);
+        this.winner(number);
+      }
+    },
+    generateNumber() {
+      let randomNumber;
+      do {
+        randomNumber = Math.floor(Math.random() * (10 - 1) + 1); //30
+      } while (this.winningNumbers.includes(randomNumber));
+      return randomNumber;
+    },
+  },
   components: { Header },
   mounted() {
-    console.log(this.getNumbers);
+    console.log(this.getNumbers),
+      setTimeout(() => {
+        this.draw();
+      }, 3000);
   },
   computed: {
     ...mapGetters({ getNumbers: ["getNumbers"] }),
@@ -52,5 +104,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.win {
+}
 </style>
